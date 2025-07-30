3d34a576318cb70ffb0b261ab4c2f24bcc3082df
Ani Pottammal <ap7564@nyu.edu>
2025-07-29 20:09:54 -0400

    valid-anagram/python

--- easy/valid-anagram/python.py
+++ easy/valid-anagram/python.py
@@ -1,5 +1,14 @@
+def valid_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    countS, countT = {}, {}
 
-def valid_anagram():
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
+
 
 if __name__ == "__main__":
-    
+    print(valid_anagram("cars", "arcs"))
+    print(valid_anagram("anirudh", "pooja"))
