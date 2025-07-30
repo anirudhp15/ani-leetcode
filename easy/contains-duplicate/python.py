22c984f162fc809804638d67804778165c2abefc
Ani Pottammal <ap7564@nyu.edu>
2025-07-29 20:16:51 -0400

    contains-duplicate/python

--- easy/contains-duplicate/python.py
+++ easy/contains-duplicate/python.py
@@ -1,5 +1,14 @@
+def contains_duplicate(nums):
+    duplicates = {}
+    for num in nums:
+        if num in duplicates:
+            return True
+        duplicates[num] = True
+    return False
 
-def contains_duplicate():
 
 if __name__ == "__main__":
-    
+    nums = [1, 2, 3, 4, 5]
+    print(contains_duplicate(nums))
+    nums2 = [1, 2, 3, 4, 5, 5]
+    print(contains_duplicate(nums2))
